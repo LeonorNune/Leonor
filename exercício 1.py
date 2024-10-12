8b78327984d5f9a32977fe6608a72991ee48bcda
LeonorNune <leonor201124@gmail.com>
2024-10-12 12:26:58 +0100

    Add files via upload

---
+++ exercício 1.py
@@ -0,0 +1,26 @@
+print("ola")
+#se eu fizer uma função  print, a consola vai escrever aquilo que pus nos parêntesis
+#------------------------- COMENTÁRIOS --------------------------
+# o comentário é uma parte do código que nao vai ser lida  pelo progama, ou seja:
+# podemos esrever anotações no código sem que elas perturbem o seu funcionamento
+# devido á SINTAXE do python
+# PYTHON - linguagem de progamação
+#
+#
+#                 gramática-- SINTAXE, ou seja, como a linguagem,FUNCIONA
+# IMPORTANTE: SEMPRE que queremos comentar alguma coisa, temos de colocar [#] ANTES e em TODAS AS LINHAS DE COMENTÁRIO
+
+palavra= "hamburguer" # MAIS DOIS TIPOS DE DADOS: VARIÁVEL ( que armazena um dado); e STRING (texto)
+# quando temos uma  STRING podemos "copiar" e "colar" o que está dentro de aspas
+print("hamburguer")
+print(palavra) #podemos usar as variáveis ( texto branco) em qualquer parte do código
+#------------------------------------------VARIÁVEIS--------------------
+# uma variável contém um valor que pode mudar, como se fosse uma caixa em que podemos pôr várias coisas (e alterá-las)
+#REGRAS:
+# NÃO PODEM começar com números!
+print(5) #número fica AZUL
+# podemos fazer contas com números
+#se uma variável começar com um NÚMERO, o código vai AUTOMATICAMENTE pensar que estamos a tratar de um NÚMERO:
+porcento=5 #NO ENTANTO, podemos definir uma VARIÁVEL com apenas um nùmero
+# isto acontece porque estamos a armazenar o valor de 5 na variável, ou seja, PORCENTO=5, MAS 5 NÃO
+#                                                                                                 PORCENTO
